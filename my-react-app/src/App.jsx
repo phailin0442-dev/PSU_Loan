3a37a6bed4cfab2feeb68c56b043d839e2e40a3b
wichisamon <Sahmimi8878@gmail.com>
2026-07-18 20:32:43 +0700

    backup before rebuild

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,31 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./login.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Login from "./pages/login";
 
 function App() {
+  const [, setUser] = useState(null);
+
+  const handleAfterLogin = (loggedInUser) => {
+    setUser(loggedInUser);
+    console.log(loggedInUser);
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      {/* route อื่นๆ */}
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Login
+              setUser={setUser}
+              goAfterLogin={handleAfterLogin}
+              goHome={() => {}}
+              goRegister={() => {}}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
